eccd4a554a20fc257ca07db45ba26ecb2d41815d
Brent Morton <MortonB@cwu.edu>
2021-07-16 08:50:13 -0700

    Initial upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MakePrintBatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBatchName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtIDs = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.chkNew = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Batch Name:";
+            // 
+            // txtBatchName
+            // 
+            this.txtBatchName.Location = new System.Drawing.Point(84, 10);
+            this.txtBatchName.Name = "txtBatchName";
+            this.txtBatchName.Size = new System.Drawing.Size(179, 20);
+            this.txtBatchName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Paste ID(s) Below:";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(12, 408);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(250, 33);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate Batch";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtIDs
+            // 
+            this.txtIDs.Location = new System.Drawing.Point(12, 69);
+            this.txtIDs.Multiline = true;
+            this.txtIDs.Name = "txtIDs";
+            this.txtIDs.Size = new System.Drawing.Size(250, 333);
+            this.txtIDs.TabIndex = 5;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 448);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(276, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // status
+            // 
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(75, 17);
+            this.status.Text = "Not Running";
+            // 
+            // chkNew
+            // 
+            this.chkNew.AutoSize = true;
+            this.chkNew.Checked = true;
+            this.chkNew.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkNew.Location = new System.Drawing.Point(16, 33);
+            this.chkNew.Name = "chkNew";
+            this.chkNew.Size = new System.Drawing.Size(102, 17);
+            this.chkNew.TabIndex = 7;
+            this.chkNew.Text = "New Cards Only";
+            this.chkNew.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 470);
+            this.Controls.Add(this.chkNew);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtIDs);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBatchName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Print Batch Creator";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBatchName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtIDs;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel status;
+        private System.Windows.Forms.CheckBox chkNew;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace MakePrintBatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string mediaType = "050";  //change this to your media type value you're checking against
+            string idPadding = "0";    //id padding, we pad with a 0, use "" if you don't pad.
+
+            string lines = txtIDs.Text.ToString();
+
+            string[] idList = lines.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            //Console.WriteLine(String.Join(",", idList));
+
+            //get job id
+            string sql = "SELECT max(JOBID)+1 FROM batchprintjobs";
+            List<string> items = new List<string>();
+            items = OraQuery(sql);
+            if (items.Count > 0)
+            {
+                string jobID = items[0];
+                //Console.WriteLine("Job ID:" + items[0].ToString());
+
+                if (idList.Length > 0)
+                {
+                    //create new batch
+                    String dateTimeStamp = DateTime.Now.ToString("MMddyyy");
+                    sql = "INSERT INTO batchprintjobs (JOBID,JOBDESCRIPTION,JOBSTATUS,CARDSTOCKTYPE,CREATEDBY,PRINTTYPE) VALUES (" + jobID + ",'" + txtBatchName.Text.ToString() + "-" + dateTimeStamp + "','I','CWU Mifare','mortonb',0)";
+                    Console.WriteLine(sql);
+                    OraQuery(sql);
+
+                    int counter = 1;
+
+                    foreach (string currID in idList)
+                    {
+                        //check that an image exists (checks db image blob size)
+                        sql = @"select * from keymappinginfo 
+                            where keyvalue = '" + idPadding + currID + @"' 
+                            and mediatype = '-1' 
+                            AND patronid in (select patronid from patronimages where dbms_lob.getlength(dbd_image) > 20)";
+                        if (chkNew.Checked)
+                        {
+                            //New cards checked, make sure patron doesn't have smart media type and not in cardlog
+                            sql += @"AND (patronid not in (select patronid from keymappinginfo where mediatype = '" + mediaType + @"') 
+                            OR patronid not in (select patronid from patroncardlog))";
+                        }
+
+                        items = OraQuery(sql);
+
+                        if (items.Count > 0)
+                        {
+                            Console.WriteLine("Patron ID:" + currID);
+                            //Build SQL String
+                            string addpatron = "INSERT INTO batchprintpatrons (PATRONID, JOBID, PRINTORDERNUM,STATUS) VALUES (getpatronid('0" + currID + "')," + jobID + "," + counter.ToString() + ",'I')";
+                            Console.WriteLine(addpatron);
+                            OraQuery(addpatron);
+                            counter++;
+                        }
+                    }
+                    status.Text = "Job created with " + (counter - 1) + " patrons.";
+                }
+                else
+                {
+                    status.Text = "No IDs in list.";
+                }
+            }
+            else
+            {
+                status.Text = "Failed to connect to Oracle?";
+            }
+            
+        }
+
+        private static List<string> OraQuery(string sql)
+        {
+            string user = ConfigurationManager.AppSettings["goldUser"];
+            string pass = ConfigurationManager.AppSettings["goldPass"];
+            string db = ConfigurationManager.AppSettings["goldDB"];
+            List<string> results = new List<string>();
+            string oradb = "Data Source="+db+";User Id="+user+";Password="+pass+";";
+            OracleConnection con = new OracleConnection(oradb);
+            try
+            {
+                con.Open();
+
+                OracleCommand cmd = new OracleCommand()
+                {
+                    Connection = con,
+                    CommandText = sql,
+                    CommandType = CommandType.Text
+                };
+                using (OracleDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        results.Add(reader[0].ToString());
+                    }
+                }
+                con.Close();
+                con.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                con.Dispose();
+            }
+
+            return results;
+        }
+    }
+}
